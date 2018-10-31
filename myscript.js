e6d62cacf5165658b3948f712bedca485dea06d0
Victor <vsomotun@gmail.com>
2018-10-31 16:58:08 -0500

    Updates and Cleaning
    Webpage is currently being updated to function as an online resume.

--- myscript.js
+++ myscript.js
@@ -1,7 +1,17 @@
-function sayHello(name) {
-    alert("Hi! " + name);
-    alert("How are you?");
-}
-function changeText(){
-  $("#greetingText").text("How ya doing?");
+/* When the user scrolls the page, execute myFunction */
+window.onscroll = function() {myFunction()};
+
+/* Get the header */
+var header = document.getElementById("myHeader");
+
+/* Get the offset position of the navbar */
+var sticky = header.offsetTop;
+
+/* Add the sticky class to the header when you reach scroll position */
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
 }
